383f30858cfe96e0aa9e661a55a95c6c4dcd788b
sumith-tech <pcsumith2200@gmail.com>
2024-05-04 12:45:27 +0530

    Intro to Middlewares

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-let express = require("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-let app = express();
-app.use((req, res, next) => {
-  console.log("inside of middleware");
+const app = express();
 
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="item"></input><input type="number" name="quantity"></input><button type="submit">Submit</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  res.send("<h1>Hello from Express</h1>");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/')
 });
+
+app.use('/',(req,res,next)=>{
+  res.send('<h1>Welcome to Express<h1>')
+})
 app.listen(3000);
